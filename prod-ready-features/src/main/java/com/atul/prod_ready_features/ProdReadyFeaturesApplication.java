6e7e9822e28aa7f0ea2fb3b399e0e965ababeb6a
the-evolver <atulsharma98535@gmail.com>
2026-02-02 02:22:22 +0530

    learning auditing

--- prod-ready-features/src/main/java/com/atul/prod_ready_features/ProdReadyFeaturesApplication.java
+++ prod-ready-features/src/main/java/com/atul/prod_ready_features/ProdReadyFeaturesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProdReadyFeaturesApplication {
 
 	public static void main(String[] args) {
+        System.out.println("Starting prod ready features ...");
 		SpringApplication.run(ProdReadyFeaturesApplication.class, args);
 	}
 
