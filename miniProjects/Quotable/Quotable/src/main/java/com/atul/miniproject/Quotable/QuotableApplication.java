57263a9a7b078d80c44d65ba2b996c2e8104aca2
the-evolver <atulsharma98535@gmail.com>
2026-02-08 19:22:30 +0530

    quotable third party api use project setup

---
+++ miniProjects/Quotable/Quotable/src/main/java/com/atul/miniproject/Quotable/QuotableApplication.java
@@ -0,0 +1,13 @@
+package com.atul.miniproject.Quotable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotableApplication.class, args);
+	}
+
+}
---
+++ miniProjects/Quotable/Quotable/src/test/java/com/atul/miniproject/Quotable/QuotableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atul.miniproject.Quotable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
